32d01b934732a3549491ed43ab3d36275bc24a68
Ali Zeaiter <ali.zeaiter@volvocars.com>
2023-03-19 00:17:50 +0100

    Add debug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { injectCopyright } from "./injectCopyright";
+import { debug } from "@actions/core";
 
 try {
+  debug("Starting copyright injection...");
   injectCopyright();
+  debug("Finished copyright injection.");
 } catch (error) {
   console.error(error);
   process.exit(1);
--- src/injectCopyright.ts
+++ src/injectCopyright.ts
@@ -3,11 +3,15 @@ import { getInputs } from "./getInputs";
 import fs from "fs/promises";
 import { getSourceFileWithCopyrightComment } from "./getSourceFileWithCopyrightComment";
 import { writeFile } from "./writeFile";
+import { debug } from "@actions/core";
 
 export async function injectCopyright() {
   const inputs = getInputs();
+  debug("Found the following inputs" + JSON.stringify(inputs, null, 2));
 
   const files = await findFiles(inputs.pattern, inputs.exclude);
+
+  debug("Found the following files" + JSON.stringify(files, null, 2));
 
   await Promise.all(
     files.map(async (file) => {
@@ -16,6 +20,8 @@ export async function injectCopyright() {
         contents,
         inputs.copyright
       );
+      debug("File:" + file);
+      debug("File content:" + contentsWithComment);
       return writeFile(file, contentsWithComment);
     })
   );
